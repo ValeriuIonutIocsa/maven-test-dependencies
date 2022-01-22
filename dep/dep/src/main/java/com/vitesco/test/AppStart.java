532c3ee8fb92e08840f0b04f5084feb08e063d67
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2022-01-22 11:02:41 +0200

    commit 4

--- dep/dep/src/main/java/com/vitesco/test/AppStart.java
+++ dep/dep/src/main/java/com/vitesco/test/AppStart.java
@@ -9,5 +9,6 @@ public class AppStart {
 			final String[] args) {
 
 		System.out.println("1111 " + LibClass.MESSAGE);
+		System.out.println("2222 " + LibClass.libMethod("Abcdef", "ABC"));
 	}
 }
--- lib/lib/src/main/java/com/vitesco/test/LibClass.java
+++ lib/lib/src/main/java/com/vitesco/test/LibClass.java
@@ -1,6 +1,14 @@
 package com.vitesco.test;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class LibClass {
 
-    public static final String MESSAGE = "message";
+	public static final String MESSAGE = "message";
+
+	public static boolean libMethod(
+            final String str,
+            final String otherStr) {
+		return StringUtils.startsWithIgnoreCase(str, otherStr);
+	}
 }
